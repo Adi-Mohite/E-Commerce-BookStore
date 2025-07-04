1a08dad1ffbd447f3aa4f6f76cdd06d635e34bb5
Adarsh Mohite <mohiteadarsh15@gmail.com>
2025-07-04 19:13:27 +0530

    Update OrdersPage.jsx

--- Frontend/src/components/OrdersPage.jsx
+++ Frontend/src/components/OrdersPage.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useAuth } from "./context/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
-const API = import.meta.env.REACT_APP_API_URL;
+const API = import.meta.env.VITE_API_URL;
 
 
 const OrdersPage = () => {
